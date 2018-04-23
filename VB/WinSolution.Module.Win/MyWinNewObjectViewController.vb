fe070591976f454852e1460515fd14afd2cba701
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WinSolution.Module.Win/MyWinNewObjectViewController.vb
@@ -0,0 +1,25 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Win.SystemModule
+
+Namespace WinSolution.Module.Win
+	Public Class MyWinNewObjectViewController
+		Inherits WinNewObjectViewController
+		Protected Overrides Sub OnAfterConstruction()
+			MyBase.OnAfterConstruction()
+		End Sub
+		Protected Overrides Sub OnActivated()
+			MyBase.OnActivated()
+			If View.ObjectTypeInfo.Type Is GetType(DomainObject1) Then
+				Me.NewObjectAction.Caption = "Add"
+			End If
+		End Sub
+		Protected Overrides Sub OnDeactivated()
+			MyBase.OnDeactivated()
+			If View.ObjectTypeInfo.Type Is GetType(DomainObject1) Then
+				Me.NewObjectAction.Caption = "New"
+			End If
+		End Sub
+	End Class
+End Namespace
